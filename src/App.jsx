1ac70c430b3922a16b8d73023ddd788a89ae0a47
HADYZELLEYA\hadi_ <hadi_scorpion95@hotmail.com>
2024-12-20 12:43:38 +0200

    routing the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
-
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import {
+  Movies,
+  MovieInformation,
+  Profile,
+  Navbar,
+  Actors,
+} from "./components/export.js";
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/export.js
@@ -0,0 +1,5 @@
+export { default as Movies } from "./movies/movies";
+export { default as MovieInformation } from "./movie Information/movieInformation";
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Actors } from "./actors/actors";
+export { default as Profile } from "./profile/Profile";
